1d531b6118f288937eda3af616e027b0f52ac75a
aaronellman@gmail.com <aaronellman@gmail.com>
2026-04-22 10:05:25 +0200

    feat: add Trade dataclass

---
+++ core/trade.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+import uuid
+
+@dataclass(slots=True)
+class Trade:
+    symbol: str
+    price: float 
+    quantity: int 
+    buyer_order_id: uuid.UUID 
+    seller_order_id: uuid.UUID
+    timestamp: datetime = field(default_factory=datetime.now)
+    id: uuid.UUID = field(default_factory=uuid.uuid4 )
