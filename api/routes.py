3b376973cbafe86856c0ddac1ef5bbcd95ec47a8
aaronellman@gmail.com <aaronellman@gmail.com>
2026-04-22 09:38:19 +0200

    fix: POST /orders add exception handling

--- api/routes.py
+++ api/routes.py
@@ -5,7 +5,9 @@ from core.order_book import OrderBook
 from core.matching_engine import MatchingEngine
 import uuid
 
+
 router = APIRouter()
+
 
 class OrderModel(BaseModel):
     price: float
@@ -13,26 +15,36 @@ class OrderModel(BaseModel):
     side: Side
     symbol: str
 
+
 class PriceLevelModel(BaseModel):
     price: float
     quantity: int
+
 
 class OrderBookResponse(BaseModel):
     asks: list[PriceLevelModel]
     bids: list[PriceLevelModel]
 
+
 order_book = OrderBook()
 matching_engine = MatchingEngine(order_book)
+
 
 def get_matching_engine():
     return matching_engine
 
+
 @router.post("/orders", status_code=201)
 def orders(order_data: OrderModel, matching_engine = Depends(get_matching_engine)):
     order = Order(**order_data.model_dump())
-    result = matching_engine.match(order)
+    
+    try:
+        result = matching_engine.match(order)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
     
     return {"matched": result, "timestamp": order.timestamp}
+
 
 @router.delete("/orders/{order_id}", status_code=200)
 def orders(order_id: uuid.UUID, matching_engine = Depends(get_matching_engine)):
@@ -42,6 +54,7 @@ def orders(order_id: uuid.UUID, matching_engine = Depends(get_matching_engine)):
     
     matching_engine.order_book.cancel_order(order)
     return {"message": "order cancelled"}
+
 
 @router.get("/orderbook", status_code=200, response_model=OrderBookResponse)
 def orders(matching_engine = Depends(get_matching_engine)):
