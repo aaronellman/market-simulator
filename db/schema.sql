6ae37cc25c4c083bfcd6744bf18c228e33a85b21
aaronellman@gmail.com <aaronellman@gmail.com>
2026-05-02 17:49:25 +0200

    chore: add PostgreSQL schema with trades table

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS trades(
+    id UUID PRIMARY KEY,
+    symbol TEXT,
+    price NUMERIC,
+    quantity INTEGER,
+    buyer_order_id UUID NOT NULL,
+    seller_order_id UUID NOT NULL
+)
