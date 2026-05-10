e5cbf51c4adb918c32614ad7c9eca0926277de9a
aaronellman@gmail.com <aaronellman@gmail.com>
2026-05-10 12:20:00 +0200

    feat: update bot state after matched trade in RandomBot

--- bots/strategies/random_bot.py
+++ bots/strategies/random_bot.py
@@ -54,21 +54,27 @@ class RandomBot(BaseBot):
 
                     query_params = {"price": price, "quantity": quantity, "side": side.value, "symbol": symbol}
                     response = await client.post(self.orders_url, json=query_params)
-                    #add _update_state() method here
+
+                    if response.json().get("matched") != []:
+                        self._update_state(symbol, quantity, side, price)
+                    
                     await sleep(self.interval)
                     continue
                 else:
                     ask_price = asks[0]["price"]
                     bid_price = bids[0]["price"]
 
+                
                 if self.balance > 0 and self.portfolio.get(symbol) == 0: #have to buy, only balance
                     price = ask_price
                     side = Side.BUY
                     quantity = self._get_trade_quantity(price, side, symbol)
+
                 elif self.balance == 0: #have to sell, no balance
                     price = bid_price
                     side = Side.SELL
                     quantity = self._get_trade_quantity(price, side, symbol)
+
                 else:   #have to choose whether to buy or sell, have balance and stocks in portfolio
                     side = self._get_random_side()
                     price = ask_price if side == Side.BUY else bid_price
@@ -76,7 +82,9 @@ class RandomBot(BaseBot):
 
                 query_params = {"price": price, "quantity": quantity, "side": side.value, "symbol": symbol}
                 response = await client.post(self.orders_url, json=query_params)
-                #add _update_state() method here
+
+                if response.json().get("matched") != []:
+                    self._update_state(symbol, quantity, side, price)
 
                 await sleep(self.interval)
                 
