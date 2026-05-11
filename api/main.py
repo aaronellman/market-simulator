cc9eba1234835ff89af1704929d61af0771bad2a
aaronellman@gmail.com <aaronellman@gmail.com>
2026-05-11 17:51:07 +0200

    feat: added lifespan function to seed market, fixed logging in main

--- api/main.py
+++ api/main.py
@@ -1,10 +1,31 @@
 from fastapi import FastAPI
-from core.order_book import OrderBook
-from core.matching_engine import MatchingEngine
-from api.routes import router
+from core.order import Order, Side
+from api.routes import router, matching_engine, SYMBOLS
 import logging
+from contextlib import asynccontextmanager
 
-logging.getLogger().setLevel(logging.INFO)
 
-app = FastAPI()
+logger = logging.getLogger()
+logging.basicConfig()
+logger.setLevel(logging.INFO)
+
+
+def supply_market(quantity: int = 1000, price: float = 10):
+    side = Side.SELL
+    
+    for symbol in SYMBOLS:
+
+        order = Order(price, quantity, side, symbol)
+        matching_engine.match(order)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    supply_market()
+    logger.info("Market Supplied")
+
+    yield
+    
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(router)
