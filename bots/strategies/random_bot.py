81e48a74c0db91b0824e01d70752d3a9fd907196
aaronellman@gmail.com <aaronellman@gmail.com>
2026-05-11 12:43:33 +0200

    fix: preventing bots from selling or buying for 0 quantity and added logic for when asks and bids are empty

--- bots/strategies/random_bot.py
+++ bots/strategies/random_bot.py
@@ -8,8 +8,8 @@ from asyncio import sleep
 class RandomBot(BaseBot):
     """creates and starts up a bot that randomly picks a stock, and chooses whether to buy or sell it"""
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, balance: float = 1000.00, base_api_url: str = "http://127.0.0.1:8000", interval: int = 1):
+        super().__init__(balance, base_api_url, interval)
 
 
     @staticmethod
@@ -47,38 +47,41 @@ class RandomBot(BaseBot):
                 asks = prices.json()["asks"]
                 bids = prices.json()["bids"]
 
-                if not asks and not bids:
+                ask_price = asks[0]["price"] if asks else None
+                bid_price = bids[0]["price"] if bids else None
+
+                if ask_price is None and bid_price is None:
                     price = self._get_random_price()
                     side = self._get_random_side()
                     quantity = self._get_trade_quantity(price, side, symbol)
-
-                    query_params = {"price": price, "quantity": quantity, "side": side.value, "symbol": symbol}
-                    response = await client.post(self.orders_url, json=query_params)
-
-                    if response.json().get("matched") != []:
-                        self._update_state(symbol, quantity, side, price)
-                    
-                    await sleep(self.interval)
-                    continue
-                else:
-                    ask_price = asks[0]["price"]
-                    bid_price = bids[0]["price"]
-
-                
-                if self.balance > 0 and self.portfolio.get(symbol) == 0: #have to buy, only balance
-                    price = ask_price
-                    side = Side.BUY
-                    quantity = self._get_trade_quantity(price, side, symbol)
-
-                elif self.balance == 0: #have to sell, no balance
-                    price = bid_price
+                    if quantity == 0:
+                        continue
+                elif ask_price is None:
+                    price = self._get_random_price()
                     side = Side.SELL
                     quantity = self._get_trade_quantity(price, side, symbol)
-
-                else:   #have to choose whether to buy or sell, have balance and stocks in portfolio
-                    side = self._get_random_side()
-                    price = ask_price if side == Side.BUY else bid_price
+                    if quantity == 0:
+                        continue
+                elif bid_price is None:
+                    price = self._get_random_price()
+                    side = Side.BUY
                     quantity = self._get_trade_quantity(price, side, symbol)
+                    if quantity == 0:
+                        continue
+                else:
+
+                    if self.balance > 0 and self.portfolio.get(symbol) == 0: #have to buy, only balance
+                        price = ask_price
+                        side = Side.BUY
+                        quantity = self._get_trade_quantity(price, side, symbol)
+                    elif self.balance == 0: #have to sell, no balance
+                        price = bid_price
+                        side = Side.SELL
+                        quantity = self._get_trade_quantity(price, side, symbol)
+                    else:   #have to choose whether to buy or sell, have balance and stocks in portfolio
+                        side = self._get_random_side()
+                        price = ask_price if side == Side.BUY else bid_price
+                        quantity = self._get_trade_quantity(price, side, symbol)
 
                 query_params = {"price": price, "quantity": quantity, "side": side.value, "symbol": symbol}
                 response = await client.post(self.orders_url, json=query_params)
