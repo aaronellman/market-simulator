0ed9e56f8ca8f4d5590b4563889d171d1ad29800
aaronellman@gmail.com <aaronellman@gmail.com>
2026-05-11 12:42:48 +0200

    fix: fixed bots using incorrect argument order in run, added method to fix async coroutine issue

--- bots/run.py
+++ bots/run.py
@@ -1,12 +1,18 @@
 from typer import Typer
 from bots.strategies.random_bot import RandomBot
-from asyncio import run, gather
+import asyncio
 strategies = {"random": RandomBot}
+
 
 app = Typer()
 
+
+async def _run_bots(coroutines):
+    await asyncio.gather(*coroutines)
+
+
 @app.command("start-bots")
-def start(count: int, strategy, interval: int = 1, starting_balance: float = 1000.00, base_api_url: str = "http://127.0.0.1:8000"):
+def start(count: int, strategy: str, interval: int = 1, starting_balance: float = 1000.00, base_api_url: str = "http://127.0.0.1:8000"):
 
     bot_instances = []
 
@@ -17,11 +23,11 @@ def start(count: int, strategy, interval: int = 1, starting_balance: float = 100
     
     for i in range(count):
         
-        bot = bot_class(base_api_url, starting_balance, interval)
+        bot = bot_class(starting_balance, base_api_url, interval)
         bot_instances.append(bot)
 
     coroutines = [bot.run() for bot in bot_instances]
-    run(gather(*coroutines))
+    asyncio.run(_run_bots(coroutines))
 
 if __name__ == "__main__":
     app()
