b96066fd0c8366c573fa83878e519fe8c08011b3
aaronellman@gmail.com <aaronellman@gmail.com>
2026-08-21 11:09:15 +0200

    fix: prevent randombot from going all-in on one trade

--- bots/strategies/random_bot.py
+++ bots/strategies/random_bot.py
@@ -1,6 +1,6 @@
 from bots.base import BaseBot
 from httpx import AsyncClient
-from random import choice, uniform
+from random import choice, uniform, betavariate
 from core.order import Side
 from asyncio import sleep
 
@@ -8,7 +8,7 @@ from asyncio import sleep
 class RandomBot(BaseBot):
     """creates and starts up a bot that randomly picks a stock, and chooses whether to buy or sell it"""
 
-    def __init__(self, balance: float = 10000.00, base_api_url: str = "http://127.0.0.1:8000", interval: int = 1):
+    def __init__(self, balance: float = 10000.00, base_api_url: str = "http://127.0.0.1:8000", interval: float = 1.0):
         super().__init__(balance, base_api_url, interval)
 
 
@@ -18,8 +18,9 @@ class RandomBot(BaseBot):
     
     
     @staticmethod
-    def _add_price_noise(price):
-        noise_multiplier = uniform(-0.05, 0.05)
+    def _add_price_noise(price, scale: float = 1.0):
+        max_noise = abs(0.05 * scale) #scalar value to adjust noise range
+        noise_multiplier = uniform(-1 * max_noise, max_noise)
         return round((1 + noise_multiplier) * price, 2)
     
     
@@ -27,15 +28,13 @@ class RandomBot(BaseBot):
 
         if side == Side.BUY:
             pending_buy_cost = sum(o["quantity"] * o["price"] for o in self.pending_orders if o["side"] == Side.BUY.value)
-            return round((self.balance - pending_buy_cost) / price, 2)
+            tradeable_quantity = (self.balance - pending_buy_cost) / price
+            return round(betavariate(2, 5) * tradeable_quantity, 8)
         else:
             pending_sell_qty = sum(o["quantity"] for o in self.pending_orders if o["side"] == Side.SELL.value and o["symbol"] == symbol)
-            
-            return self.portfolio.get(symbol, 0) - pending_sell_qty
-    
 
-    def _get_random_price(self):
-        return round(uniform(0.01, self.balance), 2)
+            tradeable_quantity = self.portfolio.get(symbol, 0) - pending_sell_qty
+            return round(betavariate(2, 5) * tradeable_quantity, 8)
 
 
     def _get_random_side(self):
@@ -46,6 +45,8 @@ class RandomBot(BaseBot):
         async with AsyncClient() as client:
             while True:
                 await sleep(self.interval)
+
+                noise_scale: float = 1.0
 
                 response = await client.get(self.symbols_url)
                 symbols = response.json()
@@ -62,31 +63,29 @@ class RandomBot(BaseBot):
                 bid_price = bids[0]["price"] if bids else None
 
                 if ask_price is None and bid_price is None:
-                    price = self._get_random_price()
+                    price = await self._get_last_price(client, symbol)
                     side = self._get_random_side()
-                    quantity = self._get_trade_quantity(price, side, symbol)
+                    noise_scale = 4.0
                 elif ask_price is None:
-                    price = self._get_random_price()
+                    price = bid_price
                     side = Side.SELL
-                    quantity = self._get_trade_quantity(price, side, symbol)
                 elif bid_price is None:
-                    price = self._get_random_price()
+                    price = ask_price
                     side = Side.BUY
-                    quantity = self._get_trade_quantity(price, side, symbol)
                 else:
 
                     if self.balance > 0 and self.portfolio.get(symbol) == 0: #have to buy, only balance
                         price = ask_price
                         side = Side.BUY
-                        quantity = self._get_trade_quantity(price, side, symbol)
                     elif self.balance == 0: #have to sell, no balance
                         price = bid_price
                         side = Side.SELL
-                        quantity = self._get_trade_quantity(price, side, symbol)
                     else:   #have to choose whether to buy or sell, have balance and stocks in portfolio
                         side = self._get_random_side()
                         price = ask_price if side == Side.BUY else bid_price
-                        quantity = self._get_trade_quantity(price, side, symbol)
+
+                price = self._add_price_noise(price, noise_scale)
+                quantity = self._get_trade_quantity(price, side, symbol)
 
                 if quantity == 0:
 
@@ -94,8 +93,6 @@ class RandomBot(BaseBot):
                     await self._poll_orders() 
                     await self._cancel_pending_orders()
                     continue
-
-                price = self._add_price_noise(price)
 
                 await self._place_order(client, price, quantity, side, symbol)
 
